c1eff92c146b7eb5a307e646fcfa6928474dd831
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2025-05-10 12:49:25 -0600

    Implement new function: data loading from file

--- equipo(4)_proyecto_final.c
+++ equipo(4)_proyecto_final.c
@@ -11,19 +11,24 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct users
+#define MAX_USERS 6
+
+typedef struct
 {
     char name[10];
     char career[7];
     int age;
-};
+} User;
 
+User users[MAX_USERS];
 
 int menu();
 void load_data_from_file();
 void find_data(int type);
 
-int main() {
+int main()
+{
+    load_data_from_file();
     while(1) {
         int opt, band = 0;
         opt = menu();
@@ -53,6 +58,42 @@ int main() {
 
 int menu() {}
 
-void load_data_from_file() {}
+void load_data_from_file()
+{
+    FILE *file = fopen("alumnos.txt", "r");
+
+    if (file == NULL)
+        printf("No se pudo abrir el archivo");
+
+    char buffer[256];
+    int totalRegisters = 0;
+
+    fgets(buffer, sizeof(buffer), file);
+    fgets(buffer, sizeof(buffer), file);
+
+    while (fgets(buffer, sizeof(buffer), file))
+    {
+        if (buffer[0] == '+')
+            continue;
+
+        char name[30], career[10];
+        int age;
+
+        sscanf(buffer, "| %29[^|] | %d | %9[^|] |", name, &age, career);
+
+        name[strcspn(name, " ")] = '\0';
+        career[strcspn(career, " ")] = '\0';
+
+        strcpy(users[totalRegisters].name, name);
+        strcpy(users[totalRegisters].career, career);
+        users[totalRegisters].age = age;
+        totalRegisters++;
+
+        if (totalRegisters >= MAX_USERS)
+            break;
+    }
+
+    fclose(file);
+}
 
 void find_data(int type) {}

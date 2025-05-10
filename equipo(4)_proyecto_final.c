adf9eeb9ffe101654acffd409edcbd95a8eeb2df
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2025-05-10 11:54:54 -0600

    Initial Project

---
+++ equipo(4)_proyecto_final.c
@@ -0,0 +1,9 @@
+/*
+    Integrantes:
+        Ozuna Salazar Jorge Alejandro
+        Pulido Moreno Hugo Nicolas
+        Ramírez Sánchez Miguel Angel
+    PROYECTO FINAL - Archivo de acceso secuencial – consultas filtradas
+    Fecha: 10 de Mayo del 2025
+*/
+
